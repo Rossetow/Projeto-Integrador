c28e99ebdc9e051b5713740c2ca7ee037b19b1bf
Rafael Rosseto <84914440+Rossetow@users.noreply.github.com>
2023-05-16 21:33:54 -0300

    Update JDImpostos.java

--- src/main/java/br/com/senac/projetointegrador/JDImpostos.java
+++ src/main/java/br/com/senac/projetointegrador/JDImpostos.java
@@ -113,10 +113,7 @@ public class JDImpostos extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       JDialog Tdialog = new JDTexto((javax.swing.JFrame)this, true);
-       
-       
-       
+    
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
